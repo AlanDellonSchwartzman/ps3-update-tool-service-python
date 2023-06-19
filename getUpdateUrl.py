a3fb0b5156a35851af96e9a63b89cabc3f25037a
Alan Dellon <alandellon0@gmail.com>
2023-06-19 14:46:19 -0300

    add game title in response

--- getUpdateUrl.py
+++ getUpdateUrl.py
@@ -11,6 +11,7 @@ def main(serial):
   url = f'https://a0.ww.np.dl.playstation.net/tpl/np/{serial}/{serial}-ver.xml'
 
   response = requests.get(url, verify=False)
+  response.encoding = 'UTF-8'
 
   if (response.status_code == 404):
     return {'error': 'Game not found'}
@@ -19,11 +20,15 @@ def main(serial):
     return ()
   
   dictionary = xmltodict.parse(response.text)
+
   packages = dictionary['titlepatch']['tag']['package']
 
   if (type(packages) == list):
-    return list(map(getUpdate, packages))
-  elif (len(packages) == 6):
-    return [getUpdate(packages)]
+    title = dictionary['titlepatch']['tag']['package'][-1]['paramsfo']['TITLE']
+    return {title: list(map(getUpdate, packages))}
+  elif (isinstance(packages, dict)):
+    title = dictionary['titlepatch']['tag']['package']['paramsfo']['TITLE']
+    print('TITLE: ', title)
+    return {title: [getUpdate(packages)]}
   else:
     return {'error': 'error'}
